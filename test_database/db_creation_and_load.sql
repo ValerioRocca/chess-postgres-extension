25c2af2e4314daebdb3a0a9fe35ecebed5ceb251
Maria Camila Salazar <146728984+mariacsalazar@users.noreply.github.com>
2023-12-10 23:19:00 +0100

    Benchmark to test scales and index

---
+++ test_database/db_creation_and_load.sql
@@ -0,0 +1,54 @@
+DROP INDEX IF EXISTS san_index_1;
+DROP INDEX IF EXISTS san_index_5;
+DROP INDEX IF EXISTS san_index_20;
+DROP INDEX IF EXISTS san_index_100;
+
+DROP TABLE IF EXISTS games_100 CASCADE;
+DROP TABLE IF EXISTS games_20 CASCADE;
+DROP TABLE IF EXISTS games_5 CASCADE;
+DROP TABLE IF EXISTS games_1 CASCADE;
+DROP EXTENSION IF EXISTS chess CASCADE; --<---- to uncomment to run our extension
+
+CREATE EXTENSION chess; --<---- also this has to be uncommented
+
+
+
+CREATE TABLE games_100 (
+    id SERIAL PRIMARY KEY,
+    notation san,
+    player VARCHAR(255),
+    game_site VARCHAR(255)
+);
+
+CREATE TABLE games_20 (
+    id SERIAL PRIMARY KEY,
+    notation san,
+    player VARCHAR(255),
+    game_site VARCHAR(255)
+);
+
+CREATE TABLE games_5 (
+    id SERIAL PRIMARY KEY,
+    notation san,
+    player VARCHAR(255),
+    game_site VARCHAR(255)
+);
+
+CREATE TABLE games_1 (
+    id SERIAL PRIMARY KEY,
+    notation san,
+    player VARCHAR(255),
+    game_site VARCHAR(255)
+);
+
+COPY games_100(notation, player, game_site)
+FROM '/home/mcsalazart/Downloads/BDMA/Systems architecture/project/test_database/csv_games_100.csv' DELIMITER ',' CSV HEADER;
+
+COPY games_20(notation, player, game_site)
+FROM '/home/mcsalazart/Downloads/BDMA/Systems architecture/project/test_database/csv_games_50.csv' DELIMITER ',' CSV HEADER;
+
+COPY games_5(notation, player, game_site)
+FROM '/home/mcsalazart/Downloads/BDMA/Systems architecture/project/test_database/csv_games_25.csv' DELIMITER ',' CSV HEADER;
+
+COPY games_1(notation, player, game_site)
+FROM '/home/mcsalazart/Downloads/BDMA/Systems architecture/project/test_database/csv_games_10.csv' DELIMITER ',' CSV HEADER;
---
+++ test_database/queries_2.sql
@@ -0,0 +1,30 @@
+
+--Query 5
+CREATE INDEX IF NOT EXISTS san_index_1 ON games_1(notation);
+CREATE INDEX IF NOT EXISTS san_index_5 ON games_5(notation);
+CREATE INDEX IF NOT EXISTS san_index_20 ON games_20(notation);
+CREATE INDEX IF NOT EXISTS san_index_100 ON games_100(notation);
+
+
+-- Consultas
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_100  WHERE hasOpening(notation, '1.e4 '::san);
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_20 WHERE hasOpening(notation, '1.e4 '::san);
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_5 WHERE hasOpening(notation, '1.e4 '::san);
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_1 WHERE hasOpening(notation, '1.e4 '::san);
+
+
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_100 WHERE hasOpening(notation, '1. Nf3 Nf6 2. c4 g6 3. Nc3 Bg7'::san);
+
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_20 WHERE hasOpening(notation, '1. Nf3 Nf6 2. c4 g6 3. Nc3 Bg7'::san);
+
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_5 WHERE hasOpening(notation, '1. Nf3 Nf6 2. c4 g6 3. Nc3 Bg7'::san);
+
+SET enable_seqscan TO OFF;
+SELECT count(*) FROM games_1 WHERE hasOpening(notation, '1. Nf3 Nf6 2. c4 g6 3. Nc3 Bg7'::san);
